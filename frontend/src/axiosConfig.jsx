8642c79c021af4ea823d126ecc5499a3032b25ac
Joy Sun <n12326836@qut.edu.au>
2025-08-17 14:57:56 +1000

    axio

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-  headers: { 'Content-Type': 'application/json' },
   //baseURL: 'http://13.236.187.98:5001', // local
   baseURL: 'http://13.236.187.98:5001', // live
   headers: { 'Content-Type': 'application/json' },
