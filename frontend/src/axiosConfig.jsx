11a82e8269f0c4aa19c9fc882daae6975728b545
n12326836 <y95.sun@connect.qut.edu.au>
2025-08-28 00:38:19 +1000

    URL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
 
   //baseURL: 'http://13.236.187.98:5001', // local
-  baseURL: 'http://13.236.187.98:5001', // live
+  baseURL: 'http://54.153.147.159:5001', // live
   //baseURL: '/api', 
   headers: { 'Content-Type': 'application/json' },
 });
