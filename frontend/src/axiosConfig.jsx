eb2fa79d971739fe2e20d3597b21a9cc32d2b8f4
n12326836 <y95.sun@connect.qut.edu.au>
2025-08-28 13:26:01 +1000

    IP remote

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
 
-  //baseURL: 'http://13.236.187.98:5001', // local
-  baseURL: 'http://54.153.147.159:5001', // live
+//baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://3.25.116.42:5001', // live
   //baseURL: '/api', 
   headers: { 'Content-Type': 'application/json' },
 });
